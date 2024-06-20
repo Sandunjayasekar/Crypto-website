b7b8e7ebb24e19ce7526bd8c043036f1b721839a
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-20 12:35:05 +0530

    api connected to spans

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd'
-
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("etherium");
+var doge = document.getElementById("dogecoin");
 var settings ={
     "async": true,
     "scrossDomain": true,
@@ -9,4 +10,8 @@ var settings ={
 }
 $.ajax(settings).done(function (response){
     console.log(response);
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+
 });
